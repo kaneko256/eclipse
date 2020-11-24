86003501adfd664c34cebc73a80c9ac549f72ae1
kaneko256 <57160296+kaneko256@users.noreply.github.com>
2020-11-24 15:06:45 +0900

    Initial commit

---
+++ src/main/java/com/github/kaneko256/git_with_eclipse/App.java
@@ -0,0 +1,13 @@
+package com.github.kaneko256.git_with_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
